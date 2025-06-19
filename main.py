03b73731b44e2dd16a0bee2e1cee0461b10c0c0c
Md Nazmul Hasan <farhapartex@gmail.com>
2025-06-20 02:23:16 +0600

    (feat) added fastapi support to have REST API

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import os
+from typing import Union
+from fastapi import FastAPI
 from dotenv import load_dotenv
+from models.query import QueryRequest
 from agents.agent import create_agent, run_query
 from lib.tools import load_csv_as_dataframe
 
@@ -9,22 +12,29 @@ OPEN_AI_MODEL = os.getenv("OPENAI_MODEL", "gpt-3.5-turbo")
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 file_path = os.getenv("FILE_PATH")
 
-if __name__ == "__main__":
-    df = load_csv_as_dataframe(file_path)
-    agent = create_agent(OPENAI_API_KEY, df)
-    print("xventory AI Agent - Inventory Management Assistant")
-    print("=" * 50)
+app = FastAPI(
+        title="xventory AI Agent",
+        description="An AI agent for inventory management",
+        version="1.0.0"
+    )
+df = load_csv_as_dataframe(file_path)
+agent = create_agent(OPENAI_API_KEY, df)
 
-    while True:
-        user_input = input("\n You: ").strip()
-        if user_input.lower() == "quit":
-            print("Goodbye!")
-            break
-        elif user_input.lower() == "memory":
-            print("Feature is on the way!")
-        else:
-            result = run_query(agent, user_input)
-            print(f"Agent: {result}")
+@app.get("/api/v1/health-check")
+async def health_check() -> Union[str, dict]:
+    try:
+        return {"response": "Working"}
+    except Exception as e:
+        return {"error": str(e)}
 
-        print("\n" + "=" * 50)
+
+@app.post("/api/v1/ask-agent")
+async def run_agent(request: QueryRequest) -> Union[str, dict]:
+
+    try:
+        result = run_query(agent, request.query)
+        return {"response": result}
+    except Exception as e:
+        return {"error": str(e)}
+
 
